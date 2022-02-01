593a4bcdf255aff281b3fbfff1c514206cce3b78
hunterbecton <hunter@hunterbecton.com>
2022-02-01 16:46:11 -0500

    Subdomain feature

--- middleware/app/withTenant.js
+++ middleware/app/withTenant.js
@@ -5,15 +5,11 @@ dbConnect();
 
 export const withTenant = async ({ req, res }) => {
   try {
-    const host = req.headers.host;
-
+    let host = req.headers.host;
     let tenantDomain;
     let tenant;
 
-    console.log(`Host: ${host}`);
-
-    // Get Tenant based on subdomain
-    // Also handle for development
+    // Get Tenant based on subdomain and also handle development
     if (
       host.includes('leappage.com') ||
       process.env.NODE_ENV === 'development'
@@ -27,15 +23,11 @@ export const withTenant = async ({ req, res }) => {
     }
     // Get Tenant based on custom domain
     else {
-      let { hostname } = new URL(req.headers.referer);
-
-      console.log(`Hostname: ${hostname}`);
-
-      hostname.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '').split('/')[0];
+      tenantDomain = host.split('.')[0];
 
       // Check if tenant exists in MongoDB
       tenant = await Tenant.findOne({
-        domain: hostname,
+        domain: tenantDomain,
       });
     }
 
