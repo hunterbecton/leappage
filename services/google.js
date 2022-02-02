c0f5e054675513d64a8e3eff2da4671a0d4e2170
hunterbecton <hunter@hunterbecton.com>
2022-02-02 15:43:12 -0500

    Add project Id to GCS

--- services/google.js
+++ services/google.js
@@ -1,6 +1,7 @@
 import { Storage } from '@google-cloud/storage';
 
 const storage = new Storage({
+  projectId: process.env.GCS_PROJECT_ID,
   credentials: {
     client_email: process.env.GCS_CLIENT_EMAIL,
     private_key: process.env.GCS_PRIVATE_KEY.replace(/\\n/g, '\n'),
