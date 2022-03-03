dd05f57e7989fefbc36211d03bff48c5c34eba34
hunterbecton <hunter@hunterbecton.com>
2022-03-03 15:27:34 -0500

    Added 404 and 500 page

--- pages/500.js
+++ pages/500.js
@@ -4,18 +4,18 @@ import { NextSeo } from 'next-seo';
 export default function Custom404() {
   return (
     <>
-      <NextSeo title='404' nofollow={true} noIndex={true} />
+      <NextSeo title='500' nofollow={true} noIndex={true} />
       <div className='h-screen'>
         <div className='min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8'>
           <div className='mx-auto max-w-max'>
             <main className='sm:flex'>
               <p className='text-4xl font-extrabold text-blue-600 sm:text-5xl'>
-                404
+                500
               </p>
               <div className='sm:ml-6'>
                 <div className='sm:border-l sm:border-gray-200 sm:pl-6'>
                   <h1 className='text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl'>
-                    Page Not Found
+                    Server Error
                   </h1>
                   <p className='mt-1 text-base text-gray-500'>
                     Please check the URL in the address bar and try again.
